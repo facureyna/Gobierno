138ba490a646755ebdf021b63262e1767605a0bb
Facundo <reynafacu5@gmail.com>
2019-06-17 23:54:59 -0300

    Estadisticas, controlador y vistas

---
+++ application/views/estadisticas.php
@@ -0,0 +1,137 @@
+<!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart, bar']});
+
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+       
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+        /*data.addRows([
+          ['Adoptados',<?php echo ($adoptados->adoptados) ?>],
+          ['No adoptados',<?php  echo ($adoptados->noAdoptados) ?>]
+          ]);*/
+
+        // Set chart options
+        var options = {'title':'Animales adoptados en el mes',
+                       'width': '500',
+                       'height': '400'};
+
+        // Instantiate and draw our chart, passing in some options.
+        var torta = new google.visualization.PieChart(document.getElementById('torta'));
+        torta.draw(data, options);
+      }
+      </script>
+      <script type="text/javascript">
+      // Set a callback to run when the Google Visualization API is loaded.
+       google.setOnLoadCallback(drawBasic);
+
+        function drawBasic() {
+        var data = google.visualization.arrayToDataTable([
+          ['Rescatistas', 'Animales',],
+          ['New York City, NY', 8175000],
+          ['Los Angeles, CA', 3792000],
+          ['Chicago, IL', 2695000],
+          ['Houston, TX', 2099000],
+          ['Philadelphia, PA', 1526000]
+        ]);
+
+        var options = {
+          title: 'Animales rescatados',
+          chartArea: {width: '50%'},
+          hAxis: {
+            title: 'Total Rescatados',
+            minValue: 0
+          },
+          vAxis: {
+            title: 'Rescatistas'
+          }
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('barras'));
+
+        chart.draw(data, options);
+      }
+      </script>
+      <script type="text/javascript">
+      google.setOnLoadCallback(drawColColors);
+
+      function drawColColors() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('timeofday', 'Time of Day');
+      data.addColumn('number', 'Motivation Level');
+      data.addColumn('number', 'Energy Level');
+
+      data.addRows([
+        [{v: [8, 0, 0], f: '8 am'}, 1, .25],
+        [{v: [9, 0, 0], f: '9 am'}, 2, .5],
+        [{v: [10, 0, 0], f:'10 am'}, 3, 1],
+        [{v: [11, 0, 0], f: '11 am'}, 4, 2.25],
+        [{v: [12, 0, 0], f: '12 pm'}, 5, 2.25],
+        [{v: [13, 0, 0], f: '1 pm'}, 6, 3],
+        [{v: [14, 0, 0], f: '2 pm'}, 7, 4],
+        [{v: [15, 0, 0], f: '3 pm'}, 8, 5.25],
+        [{v: [16, 0, 0], f: '4 pm'}, 9, 7.5],
+        [{v: [17, 0, 0], f: '5 pm'}, 10, 10],
+      ]);
+
+      var options = {
+        title: 'Motivation and Energy Level Throughout the Day',
+        colors: ['#9575cd', '#33ac71'],
+        hAxis: {
+          title: 'Time of Day',
+          format: 'h:mm a',
+          viewWindow: {
+            min: [7, 30, 0],
+            max: [17, 30, 0]
+          }
+        },
+        vAxis: {
+          title: 'Rating (scale of 1-10)'
+        }
+      };
+
+      var chart = new google.visualization.ColumnChart(document.getElementById('barras_ot'));
+      chart.draw(data, options);
+    }
+      </script>
+
+<!-- start banner Area -->
+    <section class="banner-area relative" id="home">
+      <div class="overlay overlay-bg"></div>
+      <div class="container">
+        <div class="row d-flex align-items-center justify-content-center">
+          <div class="about-content col-lg-12">
+            <h1 class="text-white">
+              Estadísticas
+            </h1>
+            <p class="text-white link-nav"><a href="<?= base_url(); ?>">Inicio </a>  <span class="lnr lnr-arrow-right"></span>  <span> estadísticas</span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End banner Area -->
+
+    <!-- Start Sample Area -->
+        <div class="row">
+            <div class="col-md-4" id="torta"></div>
+            <div class="col-md-4" id="barras"></div>
+            <div class="col-md-4" id="barras_ot"></div>
+        </div>
---
+++ application/views/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+  <!-- Mobile Specific Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="img/fav.png">
+  <!-- Author Meta -->
+  <meta name="author" content="codepixer">
+  <!-- Meta Description -->
+  <meta name="description" content="">
+  <!-- Meta Keyword -->
+  <meta name="keywords" content="">
+  <!-- meta character set -->
+  <meta charset="UTF-8">
+  <!-- Site Title -->
+  <title>Gobierno</title>
+
+  <script src="<?php echo base_url();?>assets/js/vendor/jquery-2.2.4.min.js"></script> 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="<?php echo base_url();?>assets/js/vendor/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  <script src="<?php echo base_url();?>assets/js/easing.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/hoverIntent.js"></script>
+  <script src="<?php echo base_url();?>assets/js/superfish.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.ajaxchimp.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.nice-select.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/mail-script.js"></script>
+  <script src="<?php echo base_url();?>assets/js/main.js"></script>
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/linearicons.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/magnific-popup.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/nice-select.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/animate.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.carousel.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css">
+
+  </head>
+  <body>
+      <header id="header" id="home">
+        <div class="container main-menu">
+          <div class="row align-items-center justify-content-between d-flex">
+            <div id="logo">
+              <a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>assets/img/logo.png" alt="" title="" /></a>
+            </div>
+            <nav id="nav-menu-container">
+              <ul class="nav-menu">
+                <li class="menu-active"><a href="<?php echo base_url(); ?>">Inicio</a></li>
+                <li><a href="<?php echo base_url(); ?>index.php/campanas">Campañas</a></li>
+                <li><a href="<?php echo base_url(); ?>index.php/estadisticas">Estadísticas</a></li>
+                <li><a href="contact.html">Contacto</a></li>
+              </ul>
+            </nav><!-- #nav-menu-container -->
+          </div>
+        </div>
+      </header><!-- #header -->
